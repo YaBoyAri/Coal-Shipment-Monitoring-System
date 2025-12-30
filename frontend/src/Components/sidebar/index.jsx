e299ae256d43ee8e1b8993af1dcded81b86ba040
ReefKeys <rifqilimited@gmail.com>
2025-12-30 12:18:11 +0700

    sidebar

--- frontend/src/Components/sidebar/index.jsx
+++ frontend/src/Components/sidebar/index.jsx
@@ -1,32 +1,32 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './index.css'
 
 function Sidebar() {
   return (
     <aside className="sidebar">
       <nav className="sidebar-nav">
-        <ul>
-          <li>
-            <Link to="/dashboard" className="nav-link">
-              Dashboard
-            </Link>
-          </li>
-          <li>
-            <Link to="/data-shipment" className="nav-link">
-              Data Shipment
-            </Link>
-          </li>
-          <li>
-            <Link to="/input-data-shipment" className="nav-link">
-              Input Data Shipment
-            </Link>
-          </li>
-          <li>
-            <Link to="/login" className="nav-link">
-              Login
-            </Link>
-          </li>
-        </ul>
+          <ul>
+            <li>
+              <NavLink to="/dashboard" className={({ isActive }) => 'nav-link' + (isActive ? ' active' : '')}>
+                Dashboard
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/data-shipment" className={({ isActive }) => 'nav-link' + (isActive ? ' active' : '')}>
+                Data Shipment
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/input-data-shipment" className={({ isActive }) => 'nav-link' + (isActive ? ' active' : '')}>
+                Input Data Shipment
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/login" className={({ isActive }) => 'nav-link' + (isActive ? ' active' : '')}>
+                Login
+              </NavLink>
+            </li>
+          </ul>
       </nav>
     </aside>
   )
